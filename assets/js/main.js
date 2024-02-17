01aef16226b3d7ee825a57a92cdc3ded7ad073d7
NojsNojin <raffiaksan3108@gmail.com>
2024-02-17 21:51:46 +0700

    adding sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,30 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const navList = document.querySelector(".nav-list");
 
-menuToggle.addEventListener('click', function() {
-    this.classList.toggle('active');
-    const icon = this.querySelector('i');
-  
-    if (this.classList.contains('active')) {
-      icon.classList.remove('fi-br-menu-burger'); // Hamburger class
-      icon.classList.add('fi-br-cross');   // X class
-    } else {
-      icon.classList.remove('fi-br-cross'); 
-      icon.classList.add('fi-br-menu-burger'); 
-    }
-  });
+menuToggle.addEventListener("click", function () {
+  this.classList.toggle("active");
+  const icon = this.querySelector("i");
+
+  if (this.classList.contains("active")) {
+    icon.classList.remove("fi-br-menu-burger"); // Hamburger class
+    icon.classList.add("fi-br-cross"); // X class
+  } else {
+    icon.classList.remove("fi-br-cross");
+    icon.classList.add("fi-br-menu-burger");
+  }
+});
+
+const sidebar = document.getElementById("sidebar");
+const sidebarOpenButton = document.getElementById("sidebar-menu-toggle");
+const sidebarCloseButton = document.getElementById("sidebar-close-menu-toggle");
+
+sidebarOpenButton.addEventListener("click", () => {
+  sidebar.classList.add("active");
+});
+
+sidebarCloseButton.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+});
 
 const title = document.querySelector(".about-content");
 title.classList.add("fade-in");
@@ -27,4 +39,3 @@ title.classList.add("bounce");
 setTimeout(() => {
   title.classList.remove("bounce");
 }, 2000); // Durasi animasi 2 detik
-
